58804b87a33c2f118fc9ab1d8157cb692f9ba3e4
errosh <76802442+errosh@users.noreply.github.com>
2024-01-02 10:56:05 +0530

    update port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const router = require('./routes/user');
 const path = require('path');
-const PORT = process.env.PORT || 8000;
+const PORT = 8002;
 
 const AWS = require('aws-sdk');
 
