33f71ab9a7163fad35e2531559536f76512fdc7b
errosh <76802442+errosh@users.noreply.github.com>
2024-01-03 10:28:03 +0530

    add /dev api

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.get("/hello",(req, res) => {
   res.send("Hello World updated from cicd!!!!");
 })
 
-app.get("/myapp-dev",(req, res) => {
-  res.send("Hi Roshan");
+app.get("/dev",(req, res) => {
+  res.send("This is dev server");
 })
 router.get('/', function(req, res) {
   res.sendFile('index.html', {root: path.join(__dirname, 'build')});
